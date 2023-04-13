91829913e76fff241d7baa2bd63a8ff097b11d40
Hariharan <hariharen107@gmail.com>
2023-04-14 00:35:03 +0530

    Added extra security and middlewares

--- app.js
+++ app.js
@@ -6,27 +6,53 @@ import { dirname } from 'path';
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import globalErrorHandler from './controllers/errorController.js';
-import { log } from 'console';
+import { rateLimit } from 'express-rate-limit';
+import mongoSanitize from 'express-mongo-sanitize';
+import hpp from 'hpp';
+import xss from 'xss-clean';
+import helmet from 'helmet';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1)GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-
-
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
-  next();
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
 });
+app.use('/api', limiter);
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+// Data sanitization against NoSQL query injection
 
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+app.use(express.static(`${__dirname}/public`));
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
