7f161dcf25c1688e3baf7c16648f732f35b19d0e
Hariharan <hariharen107@gmail.com>
2023-05-18 21:57:21 +0530

    Added tour and overview templates

--- app.js
+++ app.js
@@ -70,6 +70,16 @@ app.get('/', (req, res) => {
     name: 'Hari'
   });
 });
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview',{
+    title: 'All Tours'
+  });
+})
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour',{
+    title: 'The Forest Hiker Tour'
+  });
+})
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
