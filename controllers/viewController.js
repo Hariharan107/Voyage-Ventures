9775386b8f23e9d8d8e8c91fed400071f5e382cc
Hariharan <hariharen107@gmail.com>
2023-05-19 18:56:44 +0530

    Added ServerSide overview Page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,12 @@
-const getOverview = (req, res) => {
+import { Tour } from '../models/tourModel.js';
+import { catchAsync } from '../utils/catchAsync.js';
+const getOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 const getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour'
