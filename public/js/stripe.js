99eb7040c92ecbe25ae10f3d0b1431f9706303e9
Hariharan <hariharen107@gmail.com>
2023-06-06 22:24:06 +0530

    Implemeted Stripe for frontend and refactored code

---
+++ public/js/stripe.js
@@ -0,0 +1,26 @@
+// import axios from 'axios';
+// import { showAlert } from './alerts';
+
+// const stripe = Stripe(
+//   'pk_test_51NFwotSIH8fieRDzRkcDSuUbZTeeCxLIqZWZj6Ip09OND3bfRMlU8VkLdByiAIb8r0TKrN1eFtj0H82CS7osf6gV004ZTnI2R6'
+// );
+
+// const bookTour = async tourId => {
+//   try {
+//     // 1) Get checkout session from API
+//     const session = await axios(
+//       `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
+//     );
+//     console.log(session);
+//     await stripe.redirectToCheckout({
+//       sessionId: session.data.session.id
+//     });
+//     // console.log(session.data.session.url);
+//     // const stripeUrl = session.data.session.url;
+//     // window.location.href=stripeUrl;
+//   } catch (err) {
+//     console.log(err);
+//     showAlert('error', err);
+//   }
+// };
+// export { bookTour };
